098658b89cbca938eca77a9417158dce5a1a8d5c
Dmitriy Bykov <bykoffdn@gmail.com>
2020-10-07 18:33:06 +0300

    init commit

---
+++ src/components/atoms/Text.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>d-text</div>
+</template>
+
+<script>
+/**
+ * Компонент
+ *
+ * @version 1.0.0
+ * @author [Dmitriy Bykov] (https://github.com/d-darwin)
+ */
+export default {
+  name: 'DText',
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+createApp(App).mount("#app");
