ab0db51dfad07f3f5c63248eb04daa303b42c8d6
Dmitriy Bykov <bykoffdn@gmail.com>
2020-10-08 18:36:15 +0300

    text-tokens.css introduced

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <d-text>
     <p>some paragraph</p>
+    <p>some another paragraph</p>
   </d-text>
 </template>
 <script>
-import { DText } from '../index'
+import { DText } from "../index";
 
 export default {
   components: { DText }
--- src/components/atoms/Text.vue
+++ src/components/atoms/Text.vue
@@ -1,23 +1,70 @@
 <template>
-  <div><slot /></div>
+  <component
+    :is="tag"
+    :class="{
+      [`__${size}`]: size
+    }"
+    class="text"
+  >
+    <slot />
+  </component>
 </template>
 
 <script>
-
 /**
- * Component
+ * Component intents to display typography text.
  *
- * @version 1.0.0
+ * @version 0.1.0
  * @author [Dmitriy Bykov] (https://github.com/d-darwin)
  */
 export default {
-  name: "DText"
+  name: "DText",
+
+  props: {
+    /**
+     * Определяет тег компонента.<br>
+     * Принимает значения: 'div', 'span'.
+     */
+    tag: {
+      type: String,
+      default: "div"
+    },
+
+    /**
+     * Размер текста компонента.<br>
+     * Принимает значения: 'augmented', 'longread', 'general', 'small'.
+     */
+    size: {
+      type: String,
+      default: "general",
+      validator: val =>
+        ["augmented", "longread", "general", "small"].includes(val)
+    }
+  }
 };
 </script>
 
 <style scoped>
+@import "../../assets/styles/_text-tokens.css";
+
 /* special vue pseudo selector to be able to style tags inside slot */
-::v-slotted(p) {
-  font-style: italic;
+.__small ::v-slotted(*) {
+  line-height: var(--small-line-height);
+  font-size: var(--small-font-size);
+}
+
+.__general ::v-slotted(*) {
+  line-height: var(--general-line-height);
+  font-size: var(--general-font-size);
+}
+
+.__longread ::v-slotted(*) {
+  line-height: var(--longread-line-height);
+  font-size: var(--longread-font-size);
+}
+
+.__augmented ::v-slotted(*) {
+  line-height: var(--augmented-line-height);
+  font-size: var(--augmented-font-size);
 }
 </style>
