637eb9246192d48f5d89abadd5f81d169b886460
Dmitriy Bykov <bykoffdn@gmail.com>
2020-11-05 15:04:37 +0300

    Dependences clean up

--- src/tests/unit/DTypography.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from "@vue/test-utils";
-import DTypography from "@/components/containers/DTypography";
-
-describe("DTypography", () => {
-  it("Тест 1", () => {
-    const componentWrapper = mount(DTypography);
-    expect(componentWrapper.exists()).toBe(true);
-  });
-});
