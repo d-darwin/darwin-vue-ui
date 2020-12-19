ea3328dd1645a3b1e3008993bfb42c3806846dea
Dmitriy Bykov <bykoffdn@gmail.com>
2020-12-19 15:36:26 +0300

    index.js: alphabetical import/export order

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-/** utils **/
+/** core **/
 import { createApp } from "vue";
 import focusVisiblePolyfill from "focus-visible/dist/focus-visible.min.js";
-
-/** development **/
-import DGridDebug from "./components/development/DGridDebug";
-
-/** containers **/
-import DAspectRatio from "./components/containers/DAspectRatio";
-import DGrid from "./components/containers/DGrid";
-import DTypography from "./components/containers/DTypography";
-
-/** icons **/
-import DIconChecked from "./components/icons/DIconChecked";
-import DIconDirection from "./components/icons/DIconDirection";
-import DIconExternalLink from "./components/icons/DIconExternalLink";
-import DIconRadioChecked from "./components/icons/DIconRadioChecked";
-import DIconRadioUnchecked from "./components/icons/DIconRadioUnchecked";
-import DIconUnchecked from "./components/icons/DIconUnchecked";
-import DIconImage from "./components/icons/DIconImage";
-import DIconVideo from "./components/icons/DIconVideo";
-import DIconColumns from "./components/icons/DIconColumns";
-import DIconPaperclip from "./components/icons/DIconPaperclip";
-import DIconCloseCircle from "./components/icons/DIconCloseCircle";
 
 /** atoms **/
 import DButton from "./components/atoms/DButton";
@@ -44,43 +23,64 @@ import DCheckboxGroup from "./components/molecules/DCheckboxGroup";
 
 /** organisms **/
 
+/** development **/
+import DGridDebug from "./components/development/DGridDebug";
+
+/** containers **/
+import DAspectRatio from "./components/containers/DAspectRatio";
+import DGrid from "./components/containers/DGrid";
+import DTypography from "./components/containers/DTypography";
+
+/** icons **/
+import DIconChecked from "./components/icons/DIconChecked";
+import DIconCloseCircle from "./components/icons/DIconCloseCircle";
+import DIconColumns from "./components/icons/DIconColumns";
+import DIconDirection from "./components/icons/DIconDirection";
+import DIconExternalLink from "./components/icons/DIconExternalLink";
+import DIconImage from "./components/icons/DIconImage";
+import DIconPaperclip from "./components/icons/DIconPaperclip";
+import DIconRadioChecked from "./components/icons/DIconRadioChecked";
+import DIconRadioUnchecked from "./components/icons/DIconRadioUnchecked";
+import DIconUnchecked from "./components/icons/DIconUnchecked";
+import DIconVideo from "./components/icons/DIconVideo";
+
 createApp({}).use(focusVisiblePolyfill);
 
 export {
+  /** atoms **/
+  DButton,
+  DCheckbox,
+  DCopyright,
+  DError,
+  DFiles,
+  DInput,
+  DLink,
+  DLoader,
+  DPicture,
+  DRadio,
+  DSelect,
+  DSlider,
+  DSwitch,
+  DVideo,
+  /** molecules **/
+  DCheckboxGroup,
+  /** organisms **/
   /** development **/
   DGridDebug,
   /** containers **/
-  DTypography,
-  DGrid,
   DAspectRatio,
+  DGrid,
+  DTypography,
   /** icons **/
   DIconChecked,
+  DIconCloseCircle,
+  DIconColumns,
   DIconDirection,
   DIconExternalLink,
+  DIconImage,
+  DIconPaperclip,
   DIconRadioChecked,
   DIconRadioUnchecked,
   DIconUnchecked,
-  DIconImage,
-  DIconVideo,
-  DIconColumns,
-  DIconPaperclip,
-  DIconCloseCircle,
-  /** atoms **/
-  DCopyright,
-  DInput,
-  DButton,
-  DLink,
-  DSwitch,
-  DSelect,
-  DCheckbox,
-  DRadio,
-  DSlider,
-  DLoader,
-  DPicture,
-  DVideo,
-  DError,
-  DFiles,
-  /** molecules **/
-  DCheckboxGroup
-  /** organisms **/
+  DIconVideo
 };
