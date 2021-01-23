ddaef37c7e651ca848702180c932da20d0e2a1d5
Dmitriy Bykov <bykoffdn@gmail.com>
2021-01-23 18:04:39 +0300

    DButtonScrollToTop added

---
+++ src/components/atoms/DButtonScrollToTop.vue
@@ -0,0 +1,47 @@
+<template>
+  <teleport to="body">
+    <DButton
+      :icon-only="true"
+      class="d-button-scroll-to-top"
+      @click="scrollToTop"
+    >
+      <DIconArrow />
+    </DButton>
+  </teleport>
+</template>
+
+<script>
+/** utils **/
+import scrollToTop from "../../utils/scrollToTop";
+
+/** components **/
+import DButton from "./DButton";
+import DIconArrow from "../icons/DIconArrow";
+
+/**
+ * TODO:
+ *
+ * @version 1.0.0
+ * @author [Dmitriy Bykov] (https://github.com/d-darwin)
+ */
+export default {
+  name: "DButtonScrollToTop",
+
+  inheritAttrs: false,
+
+  components: { DIconArrow, DButton },
+
+  methods: {
+    scrollToTop
+  }
+};
+</script>
+
+<style scoped lang="scss">
+// @import "./assets/styles/colors";
+.d-button-scroll-to-top {
+  position: fixed;
+  bottom: 15px;
+  right: 15px;
+}
+</style>
---
+++ src/components/icons/DIconArrow.vue
@@ -0,0 +1,43 @@
+<template>
+  <svg
+    class="d-icon-arrow"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12 19V5"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M5 12L12 5L19 12"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+/**
+ * Contains svg-code.
+ *
+ * @version 1.0.0
+ * @author [Dmitriy Bykov] (https://github.com/d-darwin)
+ */
+export default {
+  name: "DIconArrow"
+};
+</script>
+
+<style scoped lang="scss">
+.d-icon-arrow {
+  path {
+    stroke: currentColor;
+  }
+}
+</style>
