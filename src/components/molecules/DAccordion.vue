4d89d24c02a7e67ee3c669f77b34ea58faf5a0ec
Dmitriy Bykov <bykoffdn@gmail.com>
2021-02-07 16:57:11 +0300

    DAccordion started

---
+++ src/components/molecules/DAccordion.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>DAccordion</div>
+</template>
+
+<script>
+/**
+ * @version 1.0.0
+ * @author [Dmitriy Bykov] (https://github.com/d-darwin)
+ */
+export default {
+  name: "DAccordion"
+};
+</script>
+
+<style scoped lang="scss">
+// @import '../../assets/styles/mixins/';
+</style>
