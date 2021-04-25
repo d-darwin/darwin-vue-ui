64b8c7bcd07987bcebbf8898a23d7d3e85b3e21b
Dmitriy Bykov <bykoffdn@gmail.com>
2021-04-25 16:51:07 +0300

    DError stories added

--- src/stories/atoms/DError.stories.js
+++ src/stories/atoms/DError.stories.js
@@ -1,5 +1,23 @@
 import { DError } from "@";
 
 export default {
-  title: "Atoms/Error"
+  title: "Atoms/Error",
+  component: DError,
+  argTypes: {
+    content: {
+      control: { type: "text" },
+      defaultValue: "Some error <strong>HTML</strong> string"
+    }
+  }
 };
+
+const Template = args => ({
+  components: { DError },
+  setup() {
+    return { args };
+  },
+  template: '<DError v-bind="args" />'
+});
+
+export const Default = Template.bind({});
+// TODO: how to check transition ???
