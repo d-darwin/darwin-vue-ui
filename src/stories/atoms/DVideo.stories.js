66cc8ab1252f62854859463f86ce4e84c2d23902
Dmitriy Bykov <bykoffdn@gmail.com>
2021-04-25 19:46:04 +0300

    DVideo stories added

---
+++ src/stories/atoms/DVideo.stories.js
@@ -0,0 +1,41 @@
+import { DVideo } from "@";
+import video from "../assets/sample-mp4-file.mp4";
+
+export default {
+  title: "Atoms/Video",
+  component: DVideo,
+  argTypes: {
+    source: {
+      control: { type: "text" }
+    },
+    caption: {
+      control: { type: "text" },
+      defaultValue: "Some caption"
+    },
+    aspectRatio: {
+      control: { type: "text" },
+      defaultValue: "9:16"
+    }
+  }
+};
+
+const Template = args => ({
+  components: { DVideo },
+  setup() {
+    return { args };
+  },
+  template: '<DVideo v-bind="args" />'
+});
+
+export const Youtube = Template.bind({});
+Youtube.args = {
+  source: "https://www.youtube.com/watch?v=ySkuZNgpd6U"
+};
+
+export const Local = Template.bind({});
+Local.args = {
+  source: video,
+  format: "video/mp4",
+  autoplay: true,
+  controls: true
+};
