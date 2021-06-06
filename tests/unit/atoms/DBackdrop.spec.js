0e87a4dc61e8e656df668f8ae09482b40e09c659
Dmitriy Bykov <bykoffdn@gmail.com>
2021-06-06 13:40:32 +0300

    Tests: DBackdrop covered

--- tests/unit/atoms/DBackdrop.spec.js
+++ tests/unit/atoms/DBackdrop.spec.js
@@ -7,4 +7,9 @@ describe("DBackdrop.vue", () => {
   it("Renders .d-backdrop element", () => {
     expect(wrapper.html().includes("d-backdrop")).toBe(true);
   });
+
+  it("Emits click event", () => {
+    wrapper.vm.clickHandler();
+    expect(!!wrapper.emitted().click).toBe(true);
+  });
 });
